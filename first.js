b41b00cca40ec36d3d195befa62644c10458b239
JackFGreen <clcortana@163.com>
2016-11-02 19:24:56 +0800

    config

--- first.js
+++
@@ -1,7 +0,0 @@
-var h2 = document.createElement('h2');
-
-h2.innerHTML = 'first.js bundle success';
-
-document.body.appendChild(h2);
-
-console.log(__dirname);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: ['./entry.js'],
+    //入口文件
+    entry: ['./src/main'],
     output: {
         path: './dist',
-        filename: 'bundle.js'
+        publicPath: '',
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[chunkhash].js'
+    },
+    // 服务器配置相关，自动刷新!
+    devServer: {
+        historyApiFallback: true,
+        hot: false,
+        inline: true,
+        grogress: true,
+    },
+    resolve: {
+        // require时省略的扩展名，如：require('module') 不需要module.js
+        extensions: ['', '.js', '.vue', 'scss'],
+        // 别名，可以直接使用别名来代表设定的路径以及其他
+        alias: {
+
+        }
     },
     module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url?mimetype=image/png&limit=10000&name=images/[name].[ext]'
-            }
-        ]
+        loaders: [{
+            test: /\.vue$/,
+            loader: 'vue'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css!autoprefixer?{browsers: ["Android 4.1", "iOS 7.1", "Chrome > 31", "ff > 31", "ie >= 10"]}'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass?sourceMap'
+        }, {
+            test: /\.(png|jpg|gif)$/,
+            loader: 'url?mimetype=image/png&limit=8192&name=images/[name].[hash].[ext]'
+        }]
     },
+
+    vue: {
+        loaders: {
+            scss: 'style!css!sass?sourceMap'
+        }
+    },
+
     plugins: [
         // new webpack.BannerPlugin('bundle.js注释')
     ]
