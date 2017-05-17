cbedd9e2229e2e5f7126c0c40c3f536bb147dd89
JackFGreen <clcortana@163.com>
2017-05-17 20:15:04 +0800

    md ajax

--- src/service/ajax.js
+++ src/service/ajax.js
@@ -16,11 +16,11 @@ import qs from 'qs';
         /*在发送请求之前做某事*/
         var type = config.method == 'get' ? 3 : 4;
         layer.loading(type)
-
         return config;
+
     }, function(error) {
         /*请求错误时做些事*/
-        layer.warning(error)
+        errHandle(error)
         return Promise.reject(error);
     });
 
@@ -28,33 +28,74 @@ import qs from 'qs';
     instance.interceptors.response.use(function(res) {
         /*对响应数据做些事*/
         layer.closeAll()
+        // res.data.message && layer.warning(res.data.message)
+        return res.data;
 
-        layer.warning(res.data.message)
-
-        return res;
     }, function(error) {
         /*请求错误时做些事*/
-        var err = error.toString().indexOf('timeout') > -1 ? '请求超时' : '网络错误';
-
-        layer.warning(err)
+        errHandle(error)
         return Promise.reject(error);
     });
 
+    var errHandle = (error) => {
+        console.log('Response', error.response);
+        console.log('Request', error.request);
+        console.log('Error', error.message);
+        console.log('Config', error.config);
+
+        var errMsg = error.message.indexOf('timeout') > -1 ? '请求超时' : '网络错误';
+
+        layer.warning('', {
+            content: errMsg + '<p class="xhr-info">' + error.message + '</p>'
+        })
+
+    }
+
     var ajax = {
+        all: axios.all,
+        spread: axios.spread,
+
         get(url, params, options) {
             var config = tool.merge({ params: params }, options);
-
             var tmp = instance.get(url, config);
-
             return tmp;
         },
         post(url, params, options) {
             // instance.defaults.headers['XXX'] = 'XXX';
             var tmp = instance.post(url, qs.stringify(params), options);
-
             return tmp;
         }
     }
 
     window.ajax = ajax;
 })(window)
+
+/**
+ * ajax.all()
+ * ajax.spread()
+ *
+function api1() {
+    return ajax.get('api')
+}
+
+function api2() {
+    return ajax.get('api')
+}
+
+function api3() {
+    return ajax.get('api')
+}
+
+ajax.all([api1(), api2(), api3()])
+    .then((res) => {
+        // @param {Array} res
+        console.log(res)
+    })
+    or
+    .then(ajax.spread((res1, res2, res3) => {
+        // @param {Object} res
+        console.log(res1)
+        console.log(res2)
+        console.log(res3)
+    }))
+ */
